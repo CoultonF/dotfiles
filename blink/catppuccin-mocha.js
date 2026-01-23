48be85bca30c3a750d2fc5f7f7caed540425d3cd
Coulton Fraser <cfraser@myijack.com>
2026-01-23 15:59:00 -0700

    feat: add OSC 8 hyperlinks, undercurl support, and Blink Shell theme

---
+++ blink/catppuccin-mocha.js
@@ -0,0 +1,25 @@
+// Catppuccin Mocha theme for Blink Shell
+// Import this in Blink Shell: Settings -> Appearance -> Themes -> Add Theme
+
+black       = '#45475a';
+red         = '#f38ba8';
+green       = '#a6e3a1';
+yellow      = '#f9e2af';
+blue        = '#89b4fa';
+magenta     = '#f5c2e7';
+cyan        = '#94e2d5';
+white       = '#bac2de';
+
+lightBlack   = '#585b70';
+lightRed     = '#f38ba8';
+lightGreen   = '#a6e3a1';
+lightYellow  = '#f9e2af';
+lightBlue    = '#89b4fa';
+lightMagenta = '#f5c2e7';
+lightCyan    = '#94e2d5';
+lightWhite   = '#a6adc8';
+
+foregroundColor = '#cdd6f4';
+backgroundColor = '#1e1e2e';
+cursorColor     = '#f5e0dc';
+selectionColor  = '#585b70';
