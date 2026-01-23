465cd17aa52ac1920540dd0b18fd88b861320f48
Coulton Fraser <cfraser@myijack.com>
2026-01-23 16:23:58 -0700

    fix: use correct HTerm format for Catppuccin Mocha Blink theme

--- blink/catppuccin-mocha.js
+++ blink/catppuccin-mocha.js
@@ -1,25 +1,23 @@
 // Catppuccin Mocha theme for Blink Shell
-// Import this in Blink Shell: Settings -> Appearance -> Themes -> Add Theme
-
-black       = '#45475a';
-red         = '#f38ba8';
-green       = '#a6e3a1';
-yellow      = '#f9e2af';
-blue        = '#89b4fa';
-magenta     = '#f5c2e7';
-cyan        = '#94e2d5';
-white       = '#bac2de';
-
-lightBlack   = '#585b70';
-lightRed     = '#f38ba8';
-lightGreen   = '#a6e3a1';
-lightYellow  = '#f9e2af';
-lightBlue    = '#89b4fa';
-lightMagenta = '#f5c2e7';
-lightCyan    = '#94e2d5';
-lightWhite   = '#a6adc8';
-
-foregroundColor = '#cdd6f4';
-backgroundColor = '#1e1e2e';
-cursorColor     = '#f5e0dc';
-selectionColor  = '#585b70';
+// https://github.com/catppuccin/catppuccin
+t.prefs_.set('color-palette-overrides',[
+  "#45475a",  // 0: Black (Surface 1)
+  "#f38ba8",  // 1: Red
+  "#a6e3a1",  // 2: Green
+  "#f9e2af",  // 3: Yellow
+  "#89b4fa",  // 4: Blue
+  "#f5c2e7",  // 5: Magenta (Pink)
+  "#94e2d5",  // 6: Cyan (Teal)
+  "#a6adc8",  // 7: White (Subtext 0)
+  "#585b70",  // 8: Bright Black (Surface 2)
+  "#f38ba8",  // 9: Bright Red
+  "#a6e3a1",  // 10: Bright Green
+  "#f9e2af",  // 11: Bright Yellow
+  "#89b4fa",  // 12: Bright Blue
+  "#f5c2e7",  // 13: Bright Magenta (Pink)
+  "#94e2d5",  // 14: Bright Cyan (Teal)
+  "#bac2de"   // 15: Bright White (Subtext 1)
+]);
+t.prefs_.set('foreground-color', "#cdd6f4");
+t.prefs_.set('background-color', "#1e1e2e");
+t.prefs_.set('cursor-color', 'rgba(245,224,220,0.7)');
