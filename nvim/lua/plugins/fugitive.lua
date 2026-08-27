2aa97db2a42e8eb37191b080b978537cb1b421e2
Coulton Fraser <cfraser@myijack.com>
2026-08-27 18:55:02 +0000

    updated models and ordering

--- nvim/lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  cmd = { "Git", "G", "Gdiffsplit", "Gvdiffsplit" },
-  keys = {
-    { "<leader>gv", "<cmd>Git<cr>", desc = "Git status (fugitive)" },
-    { "<leader>gV", "<cmd>Gvdiffsplit<cr>", desc = "Git diff split (vertical)" },
-  },
-}
